114afb31f165185fd64189eb51ededda0c8ca9e5
Jozef Zigmund <jzigmund@redhat.com>
2018-03-05 15:36:14 +0100

    Use PostgreSQL as DB instead of SQLite3
    For now defaults:
    Username: 'test'
    Database name: 'package_evolution_service'
    
    Please create user and DB on your system (with '$ createuser -d test && \
    createdb -O test package_evolution_service')

--- package_evolution_service/settings.py
+++ package_evolution_service/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'package_evolution_service.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'package_evolution_service',
+        'USER': 'test',
+        'PASSWORD': 'test',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
