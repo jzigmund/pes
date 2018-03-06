ab2692e3311e97741f9996cb43efc88d7cb37428
Jozef Mikovic <jmikovic@redhat.com>
2018-03-06 11:13:50 +0100

    update models and register them

--- pes/admin.py
+++ pes/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Package, PackageSet, Event, Release
 # Register your models here.
+admin.site.register(PackageSet)
+admin.site.register(Package)
+admin.site.register(Event)
+admin.site.register(Release)
--- pes/models.py
+++ pes/models.py
@@ -1,29 +1,38 @@
 from django.db import models
 
+
 class Package(models.Model):
     name = models.CharField(max_length=80)
-    created_at = models.DateTimeField('Date of creation')
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
 
 class PackageSet(models.Model):
     package = models.ForeignKey(Package, on_delete=models.CASCADE)
-    # needs to declare set_id (according to DB Model)
+    # TODO: set to id?
+    set_id = models.IntegerField(null=True, blank=True)
+
 
 class Release(models.Model):
     os_name = models.CharField(max_length=80)
     major_version = models.CharField(max_length=4)
     minor_version = models.CharField(max_length=6)
 
+
 class Event(models.Model):
-    # TODO: revise, not sure if this is the right way how to define and use ENUM
     ACTIONS = (
             ('0', 'New'),
             ('1', 'Removed'),
             ('2', 'Deprecated'),
             ('3', 'Replaced'),
-            ('4', 'Splitted'),
+            ('4', 'Split'),
             ('5', 'Merged')
     )
-    action = models.CharField(max_length=10, choices=ACTIONS)
+    action = models.IntegerField(choices=ACTIONS)
     release = models.ForeignKey(Release, on_delete=models.CASCADE)
-    in_packageset = models.ForeignKey(PackageSet, on_delete=models.CASCADE)
-    out_packageset = models.ForeignKey(PackageSet, on_delete=models.CASCADE, related_name='+')
+    in_packageset = models.ForeignKey(PackageSet, on_delete=models.CASCADE, related_name='in')
+    out_packageset = models.ForeignKey(PackageSet, on_delete=models.CASCADE, related_name='out')
+    description = models.TextField(blank=True, null=True)
+    docstring = models.TextField(blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
