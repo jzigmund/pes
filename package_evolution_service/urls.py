fd50b8604e7fa1505714fdb010492c976982f864
Jozef Mikovic <jmikovic@redhat.com>
2018-03-08 13:27:02 +0100

    add login and logout

--- package_evolution_service/urls.py
+++ package_evolution_service/urls.py
@@ -13,10 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib.auth.views import LogoutView, LoginView
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('login/', LoginView.as_view(redirect_authenticated_user=True)),
+    path('logout/', LogoutView.as_view()),
     path('pes/', include('pes.urls')),
     path('admin/', admin.site.urls),
 ]
